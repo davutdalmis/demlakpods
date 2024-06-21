3fbc2cc1bea017d520e0736d64a87add6ef735a5
davutdalmis <davutaras0001@gmail.com>
2024-06-21 21:07:42 +0300

    Initial Commit

---
+++ Target Support Files/Pods-demlak-demlakUITests/Pods-demlak-demlakUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_demlak_demlakUITests : NSObject
+@end
+@implementation PodsDummy_Pods_demlak_demlakUITests
+@end
---
+++ Target Support Files/Pods-demlakTests/Pods-demlakTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_demlakTests : NSObject
+@end
+@implementation PodsDummy_Pods_demlakTests
+@end
